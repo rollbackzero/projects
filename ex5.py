13a57ddf6047f8790cfd07704cd427705c9165ff
Richard Tumaliuan <richardtumaliuan@gmail.com>
2019-09-21 15:23:06 +1200

    add exercise 5

---
+++ ex5.py
@@ -0,0 +1,21 @@
+name = 'Richard Tumaliuan'
+age = 41
+height = 168
+weight = 159
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"Actually that's not too heavy")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"He's teeth are usually {teeth} depending on the coffee")
+
+# this line is tricky, try to get it right exactly
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}")
+
+
