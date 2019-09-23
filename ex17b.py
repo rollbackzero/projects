b968d63c17435d72c6a1d29c9d8810a4f1f7a551
Richard Tumaliuan <richardtumaliuan@gmail.com>
2019-09-23 23:01:48 +1200

    updated exercises

--- ex17b.py
+++ ex17b.py
@@ -3,20 +3,13 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-infile = open(from_file)
-indata = infile.read()
+indata = open(from_file).read()   # open source file, read the contents and assign to object file
 
-print(f"Copying from {from_file} to {to_file}")
-print(f"The input file is {len(indata)} bytes long")
-print(f"Does the output file exist? {exists(to_file)}")
-print("Ready, hit RETURN to continue, CTRL-C to abort")
-input()
+print(f'''Copying from {from_file} to {to_file}
+The input file is {len(indata)} bytes long  
+Copying..........
+''')
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
+out_file = open(to_file, 'w').write(indata)  # open dst file and copy contents of source file
+  
 print("Alright, all done")
-
-
-
-
