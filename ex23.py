d8e6c2332a09c0ce538d97e3321fa162fad91b75
Richard Tumaliuan <richardtumaliuan@gmail.com>
2019-10-01 22:50:07 +1300

    new exercises

--- ex23.py
+++ ex23.py
@@ -2,19 +2,19 @@ import sys
 script, input_encoding, error = sys.argv
 
 def main(language_file, encoding, errors):
-    line = language_file.readline()
+    line = language_file.readline()  
 
     if line:
-        print_line(line, encoding, errors)
-        return main(language_file, encoding, errors)
+        print_line(line, encoding, errors)  
+        return main(language_file, encoding, errors)  
 
-def print_line(line, encoding, errors):
-    next_lang = line.strip()
-    raw_bytes = next_lang.encode(encoding, errors=errors)
-    cooked_string = raw_bytes.decode(encoding, errors=errors)
+def print_line(line, encoding, errors):    
+    next_lang = line.strip()  # strip trailing \n on the line string  
+    raw_bytes = next_lang.encode(encoding, errors=errors)  # encode strings to bytes
+    cooked_string = raw_bytes.decode(encoding, errors=errors)  # decode bytes to string
 
-    print(raw_bytes, "<===>", cooked_string)
+    print(raw_bytes, "<===>", cooked_string)  
 
-languages = open("languages.txt", encoding="utf-8")
+languages = open("languages.txt", encoding="utf-8")   
 
-main(languages, input_encoding, error)
+main(languages, input_encoding, error)  
