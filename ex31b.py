a01dcf6769e1e5614587abec22cc5871538c83e0
Richard Tumaliuan <richardtumaliuan@gmail.com>
2019-10-04 21:10:55 +1300

    new exercises

---
+++ ex31b.py
@@ -0,0 +1,43 @@
+print('''You enter a dark room with two doors.
+Do you go through door# 1 or door# 2?''')
+
+door = input('> ')
+
+if door == '1':
+    print("There's a beautiful woman sitting on the sofa")
+    print("What do you do?")
+    print("1. Talk to her?")
+    print("2. Fuck her gently")
+    
+    woman = input('> ')
+    if woman == '1':
+        print("Your a good lad. Good job")
+    elif woman == '2':
+        print("Nice, fuck her hard")
+    else:
+        print(f"maybe option {woman} is the right thing to do")
+
+elif door == '2':
+    print("There's a young lady wearing a bikini")
+    print("What do you do?")
+    print("1. Tell her to get on her knees?")
+    print("2. Fuck her from behind?")
+
+    lady = input('> ')
+    if lady == '1' or lady == '2':
+        print("Tell her to swallow everything")
+        print("Hold her hips and fuck her dog style")
+    else:
+        print(f"Maybe {lady} is much better idea")
+
+else:
+    print("Common lets see a movie")
+
+
+
+
+
+        
+
+        
+
