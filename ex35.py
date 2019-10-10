7a8980ddc08d446b108a175a63131e5871c52d5d
Richard Tumaliuan <richardtumaliuan@gmail.com>
2019-10-10 23:22:25 +1300

    updated ex35.py

--- ex35.py
+++ ex35.py
@@ -4,7 +4,7 @@ def gold_room():
     print('This room is full of gold. How much do you take?')
 
     choice = input('> ')
-    if '0' in choice or '1' in choice:
+    if choice != '' and choice != '0':
         how_much = int(choice)
     else:
         dead('Man, learn to type a number')
@@ -70,10 +70,3 @@ def start():
         dead('You stumble around the room until you starve')
 
 start()
-
-
-
-
-
-
-
