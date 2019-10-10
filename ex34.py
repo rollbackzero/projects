5eadd8fa698c272f78358c8023f5822f8db607b7
Richard Tumaliuan <richardtumaliuan@gmail.com>
2019-10-10 21:26:32 +1300

    lists

---
+++ ex34.py
@@ -0,0 +1,9 @@
+animals = ['Horse', 'Dog', 'Cow', 'Cat', 'Elephant', 'Zebra', 'Tiger', 'Lion', 'Cheetah', 'Puma']
+
+card = int(input('Please select cardinal number: '))
+
+print(f'Cardinal order: {animals[card]}')
+
+ord = card - 1
+
+print(f'Ordinal order: {animals[ord]}')
