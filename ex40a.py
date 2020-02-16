6b7d3c800a5ee44f4a2dfbb65ea51b34db4d73ea
Richard Tumaliuan <richardt@Richards-MacBook-Pro-2.local>
2020-02-16 16:24:53 +1300

    updated exercise 40a

--- ex40a.py
+++ ex40a.py
@@ -1,6 +1,20 @@
 #mystuff = {'apple': "I AM APPLES"}
 #print(mystuff['apple'])
 
-import mystuff
-mystuff.apple()
-print(mystuff.tangerine)
+#import mystuff
+#mystuff.apple()
+#print(mystuff.tangerine)
+
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "and now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES")
+
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
