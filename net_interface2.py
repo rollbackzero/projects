607e6d87cc8487a1fcddf877291d4090c2c71330
Richard Tumaliuan <richardt@Richards-MacBook-Pro-2.local>
2020-04-21 23:48:44 +1200

    net interface2

--- net_interface2.py
+++ net_interface2.py
@@ -6,21 +6,11 @@ import yaml
 ENV = Environment(loader=FileSystemLoader('/Users/richardt/src/projects/'))
 
 def get_interface_speed(interface_name):
-    """
-    get_interface_speed returns the default Mbps value for a given
-    network interface by looking for certain keywords in the name.
-    also convert to lowercase and check for a string match on the description.
-    """
-
     if 'gigabit' in interface_name.lower():
         return 1000
     if 'fast' in interface_name.lower():
         return 100
 
-# Filters are added to the ENV object after declaration. Note that we're
-# actually passing in our "get_interface_speed" function and not running
-# it -- the template engine will execute this function when we call the 
-# template.render()
 ENV.filters['interface_speed'] = get_interface_speed
 template = ENV.get_template('template2.j2') 
 
